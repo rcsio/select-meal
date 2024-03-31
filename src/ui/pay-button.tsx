82e63d888f925d7fa1e6ef0bca2d6ecd9c76912d
rcsio <78203730+rcsio@users.noreply.github.com>
2024-04-01 03:28:20 +0400

    Disable pay button if no items

--- src/ui/pay-button.tsx
+++ src/ui/pay-button.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import useApp from "@/hooks/useApp";
 import clsx from "clsx";
 import { useState } from "react";
 
 export default function PayButton() {
   const [hovered, setHovered] = useState(false);
+  const { selections } = useApp();
 
   function handleClick() {
     alert(
@@ -14,15 +16,16 @@ export default function PayButton() {
 
   return (
     <button
+      disabled={selections.length === 0}
       onClick={handleClick}
-      onMouseEnter={(e) => setHovered(true)}
-      onMouseLeave={(e) => setHovered(false)}
-      className="flex h-14 w-[407px] items-center justify-center gap-x-3 rounded-full bg-brand-indigo text-white"
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      className="flex h-14 w-[407px] items-center justify-center gap-x-3 rounded-full bg-brand-indigo text-white transition-colors ease-in-out disabled:cursor-not-allowed disabled:bg-gray-500"
     >
       <span className="text-lg font-medium leading-[21px]">Payment</span>
       <i
         className={clsx(
-          "fi fi-rr-arrow-right text-2xl transition-transform",
+          "fi fi-rr-arrow-right text-2xl transition-transform ease-in-out",
           hovered && "translate-x-1",
         )}
       />
